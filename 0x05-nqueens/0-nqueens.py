261efd900276f29c6e75795cd877aba68b57a9e8
Idongesit Ekanem <aidisapp94@gmail.com>
2024-10-18 22:11:26 +0100

    Logic changes and fixes

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -3,6 +3,8 @@
 N-Queens problem solver
 """
 
+import sys
+
 def is_safe(board, row, col, n):
     """Check if a queen can be placed at board[row][col]"""
     for i in range(col):
@@ -48,13 +50,16 @@ def solve_n_queens(n):
         print(solution)
 
 if __name__ == "__main__":
-    import sys
-
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
 
-    n = int(sys.argv[1])
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
     if n < 4:
         print("N must be at least 4")
         sys.exit(1)
