0803bc6be0a2d985c0521356c6554a913152426f
Idongesit Ekanem <aidisapp94@gmail.com>
2024-10-30 13:50:23 +0100

    I sorted out installations issues and also fixed the output errors that I faced

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,16 +2,32 @@
 
 const request = require('request');
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+// Get the movie ID from command line arguments
+const movieId = process.argv[2];
+
+// Make a request to the /films/ endpoint using the provided movie ID
+request(`https://swapi.dev/api/films/${movieId}/`, (err, res, body) => {
   if (err) throw err;
-  const actors = JSON.parse(body).characters;
-  exactOrder(actors, 0);
+
+  // Parse the response body to get character URLs
+  const characters = JSON.parse(body).characters;
+
+  // Call the function to print character names in order
+  printCharacterNames(characters, 0);
 });
-const exactOrder = (actors, x) => {
-  if (x === actors.length) return;
-  request(actors[x], function (err, res, body) {
+
+// Function to print character names recursively
+const printCharacterNames = (characters, index) => {
+  if (index === characters.length) return; // Stop condition
+
+  // Request each character's data
+  request(characters[index], (err, res, body) => {
     if (err) throw err;
+
+    // Print the character name
     console.log(JSON.parse(body).name);
-    exactOrder(actors, x + 1);
+    
+    // Recursive call to process the next character
+    printCharacterNames(characters, index + 1);
   });
 };
