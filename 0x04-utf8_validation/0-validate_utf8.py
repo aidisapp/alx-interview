286062a02e0f2f17ba8809ca4f64dba85c7d4cdd
Idongesit Ekanem <aidisapp94@gmail.com>
2024-10-30 13:04:25 +0100

    Tried to restructure the logic of the code so that this file defines the validUTF8 function

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,44 +1,25 @@
 #!/usr/bin/python3
+"""
+This module defines the validUTF8 function.
+"""
 
-""" This script reads stdin line by line and computes metrics"""
+def validUTF8(data):
+    """Check if a list of integers represents a valid UTF-8 encoding."""
+    num_bytes = 0
 
-import sys
+    for num in data:
+        if num_bytes == 0:
+            if (num >> 5) == 0b110:
+                num_bytes = 1
+            elif (num >> 4) == 0b1110:
+                num_bytes = 2
+            elif (num >> 3) == 0b11110:
+                num_bytes = 3
+            elif (num >> 7):
+                return False
+        else:
+            if (num >> 6) != 0b10:
+                return False
+            num_bytes -= 1
 
-
-def printsts(dic, size):
-    """ Prints information """
-    print("File size: {:d}".format(size))
-    for i in sorted(dic.keys()):
-        if dic[i] != 0:
-            print("{}: {:d}".format(i, dic[i]))
-
-
-sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-       "404": 0, "405": 0, "500": 0}
-
-count = 0
-size = 0
-
-try:
-    for line in sys.stdin:
-        if count != 0 and count % 10 == 0:
-            printsts(sts, size)
-
-        stlist = line.split()
-        count += 1
-
-        try:
-            size += int(stlist[-1])
-        except ValueError:
-            pass
-
-        try:
-            if stlist[-2] in sts:
-                sts[stlist[-2]] += 1
-        except IndexError:
-            pass
-    printsts(sts, size)
-
-except KeyboardInterrupt:
-    printsts(sts, size)
-    raise
+    return num_bytes == 0
