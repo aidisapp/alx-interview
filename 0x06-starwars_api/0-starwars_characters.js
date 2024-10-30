82ae588f8d7f3618e3543954bd677bbac5b9824d
Idongesit Ekanem <aidisapp94@gmail.com>
2024-10-30 13:56:39 +0100

    I fixed ESLint error issues

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,15 +6,15 @@ import request from 'request';
 const movieId = process.argv[2];
 
 // Make a request to the /films/ endpoint using the provided movie ID
-request(`https://swapi.dev/api/films/${movieId}/`, (err, res, body) => {
-  if (err) throw err;
+request(`https://swapi.dev/api/films/${movieId}/`, function (err, _res, body) {
+        if (err) throw err;
 
-  // Parse the response body to get character URLs
-  const characters = JSON.parse(body).characters;
+        // Parse the response body to get character URLs
+        const characters = JSON.parse(body).characters;
 
-  // Call the function to print character names in order
-  printCharacterNames(characters, 0);
-});
+        // Call the function to print character names in order
+        printCharacterNames(characters, 0);
+    });
 
 // Function to print character names recursively
 const printCharacterNames = (characters, index) => {
